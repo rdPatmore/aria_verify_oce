d349a165c322573d0752cfa878491d3fa679572a
rdPatmore <ryapat@noc.ac.uk>
2026-04-09 10:31:23 +0100

    set as package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+          name="VERIFY",
+          version="0.1",
+          packages=["Earthmover","Plotting"]
+         )
+
